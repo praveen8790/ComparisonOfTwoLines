78d6f26890125afd8a77e5898a638cc0afe5df77
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-18 02:26:49 +0530

    objects are created to implement the program

--- src/main/java/Assignment/LineCompare/Main.java
+++ src/main/java/Assignment/LineCompare/Main.java
@@ -3,10 +3,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
+
 public class Main {
 	private static final Logger LOG = LogManager.getLogger(Main.class);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation program");
+		LineLength len = new LineLength();
+		Double l1=len.length(9.8, 9.8, 1.8, 1.8);
+		Double l2=len.length(20.5, 20.5, 2.9, 2.9);
+		CheckLineEquality equality = new CheckLineEquality();
+		Boolean boolvalue = equality.equality(l1, l2);
+		if(boolvalue.booleanValue()==false) {
+			LinesComparison lines = new LinesComparison();
+			String s = lines.compareLength(l1, l2);
+			LOG.debug(s);
+		}
+		else {
+			LOG.debug("both lines are equal");
+		}
+		
 		
 	}
 
