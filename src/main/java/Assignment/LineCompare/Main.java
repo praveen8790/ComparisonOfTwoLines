579c12e80345cf753fbe3732da40ebd720728f84
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-18 02:04:23 +0530

    welcome to Line Comparison Program

---
+++ src/main/java/Assignment/LineCompare/Main.java
@@ -0,0 +1,13 @@
+package Assignment.LineCompare;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Main {
+	private static final Logger LOG = LogManager.getLogger(Main.class);
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation program");
+		
+	}
+
+}
