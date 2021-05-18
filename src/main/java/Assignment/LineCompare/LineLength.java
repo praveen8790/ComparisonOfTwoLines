5838bf0d68a1a99e6eda5de704a554e917501acf
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-18 09:04:17 +0530

    made changes to user input by implementing scanner

--- src/main/java/Assignment/LineCompare/LineLength.java
+++ src/main/java/Assignment/LineCompare/LineLength.java
@@ -1,7 +1,21 @@
 package Assignment.LineCompare;
+import java.util.Scanner;
 
 public class LineLength {
-	public static double length(double x1, double y1, double x2, double y2) {
+	public static double length() {
+		double x1,x2,y1,y2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("........coordinates of line........");
+		System.out.println("enter value of start point coordinate X: ");
+		x1 = scanner.nextDouble();
+		System.out.println("enter value of start point coordinate Y: ");
+		y1 = scanner.nextDouble();
+		System.out.println("enter value of end point coordinate X: ");
+		x2 = scanner.nextDouble();
+		System.out.println("enter value of end point coordinate Y: ");
+		y2 = scanner.nextDouble();
+		System.out.println("the given coordinates are ("+x1+","+y1+"),("+x2+","+y2+")" );
+		
 		return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 	}
 }
--- src/main/java/Assignment/LineCompare/Main.java
+++ src/main/java/Assignment/LineCompare/Main.java
@@ -3,14 +3,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-
 public class Main {
 	private static final Logger LOG = LogManager.getLogger(Main.class);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation program");
 		LineLength len = new LineLength();
-		Double l1=len.length(9.8, 9.8, 1.8, 1.8);
-		Double l2=len.length(20.5, 20.5, 2.9, 2.9);
+		Double l1=len.length();
+		Double l2=len.length();
 		CheckLineEquality equality = new CheckLineEquality();
 		Boolean boolvalue = equality.equality(l1, l2);
 		if(boolvalue.booleanValue()==false) {
